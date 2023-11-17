20fac245ecd583a74d3e666abd2670022d987f47
shiffygreen <shiffygreen73@gmail.com>
2023-11-16 21:35:18 -0500

    1st commit for trial branch

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,6 +1,9 @@
 use humanbotydb
 go 
+drop table if exists body
+go 
 create table dbo.body(id int not null identity primary key, 
-    bodypart varchar(100) not null, 
+    creaturetype varchar(25) not null,
+	bodypart varchar(100) not null, 
     num int not null
     )
--- 2 sample data/4 sample data.sql
+++
@@ -1,3 +0,0 @@
---how do you store body parts that dont have a fixed num?
-insert body(bodypart, num)
-select 'hair', 4000
